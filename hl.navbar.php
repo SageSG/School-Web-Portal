3adc02c2c5d31f8ee3f055bfde5b04cb2b243a5f
SageSG <houliangyip@gmail.com>
2020-11-07 19:32:31 +0800

    First commit

---
+++ hl.navbar.php
@@ -0,0 +1,57 @@
+<nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark">
+    <a class="navbar-brand" href="hl.index.php">
+        <img src="images/sit.png" width="50" height="50" alt="Singapore Institute of Technology"/>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Courses</a>
+            </li>
+            <li class="nav-item">
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Student's Life
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Sports & Adventure</a>
+                    <a class="dropdown-item" href="#">Arts and Culture</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">CCA Compatibility Survey Form</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact Us</a>
+            </li>
+            <!--Disabled Items-->
+            <!--      <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                  </li>-->
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                 <!--Search form--> 
+                <form class="form-inline ml-auto">
+                    <div class="md-form my-0">
+                        <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+                    </div>
+                    <button href="#!" class="btn btn-outline-light btn-md my-0 ml-sm-2" type="submit"><i class="fas fa-search" aria-hidden="true"></i></button>
+                </form>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!--Login Links-->
+<!--    <div class ="login">
+            <li>
+                <a href="#!" class="nav-link" type="submit">Login<i class='fas fa-door-open login_logo'></i></a>
+            </li>
+    </div>-->
