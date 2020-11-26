2cb84eb38132bbad6854526a3768fc5a0104c5dc
schoolaries97 <73827618+schoolaries97@users.noreply.github.com>
2020-11-27 00:32:38 +0800

    Update ccauser.php

--- ccauser.php
+++ ccauser.php
@@ -5,14 +5,16 @@ $userClass = new userClass();
 $userDetails=$userClass->userDetails($_SESSION['uid']);
 include('session.php');
 ?>
+<!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<link rel="stylesheet" type="text/css" href="css\style.css" charset="utf-8" />
+
 <html lang="en">
     <head>
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
         <?php
         include "head.php";
         ?>
@@ -23,11 +25,20 @@ and open the template in the editor.
         include "navbar.php";
         ?> 
     <main class="container">          
-	
+        <div>
 <div class="settings">
   <h1>CCA Record</h1>
       </div>
-        <div>
+        <table class='table'>
+        <thead>
+        <tr>
+            <th scope='col'>Username</th>
+            <th scope='col'>Email</th>
+            <th scope='col'>CCA</th>
+        </tr>
+        </thead>
+        
+        
  <?php
     $servername = "localhost";
     $username = "sqldev";
@@ -40,35 +51,30 @@ and open the template in the editor.
     $query=$conn->prepare("select * from users, basketball where users.uid = basketball.uid;");
     $query->execute();
     $query->bind_result($uid,$username,$password,$email,$name,$profile_pic,$google_auth_code,$bid,$bname,$uid  );
-        echo "<table class='table'>";
-        echo "<thead>";
-        echo "<tr>";
-        echo "<th scope='col'>Username</td>";
-        echo "<th scope='col'>Email</td>";
-        echo "<th scope='col'>CCA</td>";
-        echo "</tr>";
-        echo "</thead>";
-        echo "<tbody>";
     while($query->fetch())
     {
             if ($uid == $session_uid)
             {
+            echo "<tbody>";
             echo "<td scope='col'>".$username."</td>";
             echo "<td scope='col'>".$email."</td>";
             echo "<td scope='col'>".$bname."</td>";
             echo "</tr>";	
             echo "</tbody>";
-            echo "</table><br><br>";
             }
     }
     $query->close();
     
 ?>
-        </div>
+        </table><br><br><br><br><br><br><br><br>
+    </div>
     </main>
-    <?php
-    include "footer.php";
-    ?>
+        <br><br>
+            <div id="footer">
+                <?php
+                include "footer.php";
+                ?>
+            </div>
 </body>
 </html>
 
