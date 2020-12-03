11069703ba8c6eb6eb8a6f1c44f42b0007e693d1
schoolaries97 <73827618+schoolaries97@users.noreply.github.com>
2020-12-04 05:57:10 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,11 +4,6 @@ $session_uid='';
 $session_googleCode='';
 $_SESSION['uid']=''; 
 $_SESSION['googleCode']='';
-//if(empty($session_uid) && empty($_SESSION['uid']))
-//{
-//$url=BASE_URL.'index.php';
-//header("Location: $url");
-//}
 session_unset();
 session_destroy();
 ?>
