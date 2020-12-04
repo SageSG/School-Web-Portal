3c8c68d7d3e4929a515bf3c164a895ca2099b8a7
SageSG <houliangyip@gmail.com>
2020-12-04 18:57:33 +0800

    remove comment

--- session.php
+++ session.php
@@ -4,10 +4,4 @@ if(!empty($_SESSION['uid']) && !empty($_SESSION['googleCode']))
 	$session_uid=$_SESSION['uid'];
 	$session_googleCode=$_SESSION['googleCode'];
 }
-
-//if(empty($session_uid) && empty($session_googleCode))
-//{
-//	$url=BASE_URL.'index.php';
-//	header("Location: $url");
-//}
 ?>
